7990de5bbd1ff2612aaae2ee1942003b0e671caf
jbericat <jbericat@uoc.edu>
2020-09-30 23:50:31 +0200

    v_0.2

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ modules:
 endif
 
 exit_code: extras/exit_code.c
-	gcc -static -D__KERNEL__ -I../../$(KERNEL_VERSION)/include exit_code.c -o exit_code
+	gcc -static -D__KERNEL__ -I../../$(KERNEL_VERSION)/include extras/exit_code.c -o extras/exit_code
 
 clean:
 	rm -rf *.[oas] .*.flags *.ko .*.cmd .*.d .*.tmp *.mod.c .tmp_versions Module.symvers
--- traceexit.c
+++ traceexit.c
@@ -7,7 +7,13 @@
 #include<linux/seq_file.h>
 #include<linux/slab.h>
 
+#define BUFSIZE  100
 #define PROC_FILE "traceexit" //const char *HELLO2 = "Howdy";
+#define GPF_DISABLE write_cr0(read_cr0() & (~ 0x10000)) /* Disable read-only protection */
+#define GPF_ENABLE write_cr0(read_cr0() | 0x10000) /* Enable read-only protection */
+
+
+/******* 1. PROC FS OPERATIONS IMPLEMENTATION **********/
 
 static char *str = NULL;
 
@@ -42,25 +48,95 @@ static struct file_operations my_fops={
 	.write = my_proc_write
 };
 
-static int __init hello_init(void){
-	struct proc_dir_entry *entry;
-	entry = proc_create("PROC_FILE",0777,NULL,&my_fops);
-	if(!entry){
+/*********************** END (1) ************************/
+
+/******* 2. CUSTOM EXIT SYSCALL IMPLEMENTATION **********/
+
+extern unsigned sys_call_table[];
+
+asmlinkage long (*original_sys_exit)(int) = NULL;
+
+/* new syscall */
+asmlinkage long
+new_sys_exit(int exit_code) 
+{
+  /*TO-DO: Read from /proc/traceexit in order to check if "code" exists. If so,
+  we read its counter value, we increment it by 1 and then we write it back 
+  to the /proc/traceexit file corresponding "code" field. */
+  
+  /* 2.1. Read from /proc/traceexit */
+
+  // myread() .....
+
+  /* 2.2. check if code exists & update count value */
+
+  /* 2.3. Write count value back to /proc/traceexit */
+
+  // TO-DO: Implement sys_write system call- > Accessing  
+  // /proc/traceexit will trigger the write operation implementation ( myWrite() ) 
+  char write_ubuf[BUFSIZE];
+  int c = 0;
+  int exit_counter = 0;
+  c = strlen(write_ubuf);
+  sscanf(write_ubuf,"%d %d",&exit_code,&exit_counter);
+  //my_proc_write(PROC_FILE,write_ubuf,c,NULL);
+
+  /* 2.4. Close file, print message and execute exit syscall */
+
+  printk ("exit code %d captured at /proc/traceexit\n", exit_code);
+  return original_sys_exit(exit_code);
+}
+
+/*********************** END (2) ************************/
+
+/****************** 3. MODULE OPERATIONS ****************/
+
+static int __init traceexit_init(void){
+
+   /* create the /proc file */
+   struct proc_dir_entry *entry;
+   entry = proc_create(PROC_FILE,0777,NULL,&my_fops);
+   if(!entry){
 		return -1;	
-	}else{
+   }else{
 		printk(KERN_INFO "create proc file successfully\n");
-		printk (KERN_INFO "Exit syscall \"bypass\" correctly installed\n Compiled at %s %s\n", __DATE__,
-          __TIME__);
+		printk (KERN_INFO "Exit syscall \"bypass\" correctly installed\n Compiled at %s %s\n", __DATE__, __TIME__);
 		printk (KERN_NOTICE "exit syscall captured");
-	}
-	return 0;
+   }
+
+   /* Enable custom exit syscall */
+   original_sys_exit = (asmlinkage long (*)(int))(sys_call_table[__NR_exit]);
+  
+   GPF_DISABLE; /* Disable read-only protection (sys_call_table is on a read-only page )*/
+   sys_call_table[__NR_exit] = (unsigned) new_sys_exit;
+   GPF_ENABLE; /* Enable read-only protection */
+ 
+   printk (KERN_NOTICE "exit syscall captured");
+   printk (KERN_INFO "Correctly installed\n Compiled at %s %s\n", __DATE__,
+          __TIME__);
+   return (0);
+
 }
 
-static void __exit hello_exit(void){
-	remove_proc_entry("PROC_FILE",NULL);
-	printk(KERN_INFO "exit syscall restored\n");
+static void __exit traceexit_exit(void){
+  /* Remove procfs entry */
+	remove_proc_entry(PROC_FILE,NULL);
+
+  /* Restore previous state */
+  if (sys_call_table[__NR_exit] == (unsigned) new_sys_exit) {
+    GPF_DISABLE; /* Disable read-only protection (sys_call_table is on a read-only page )*/
+    sys_call_table[__NR_exit] = (unsigned) original_sys_exit;
+    GPF_ENABLE; /* Enable read-only protection */
+
+    printk (KERN_NOTICE "exit syscall restored\n");
+  }
+  else 
+    printk (KERN_ALERT "Unexpected error\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(traceexit_init);
+module_exit(traceexit_exit);
 MODULE_LICENSE("GPL");
+
+
+/*********************** END (3) ************************/
