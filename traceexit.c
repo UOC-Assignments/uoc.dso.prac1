50fae1f40497023c9c142f6770c00caf28e1c83c
jbericat <jbericat@uoc.edu>
2020-10-02 19:44:26 +0200

    v 1.1

--- traceexit.c
+++ traceexit.c
@@ -1,62 +1,73 @@
 /*
-
-				#################################################
-				#                                               #
-				#     	UOC - Open University of Catalonia      #
-				#                                               #
-				#     	  OPERATIVE SYSTEMS DESIGN (DSO)        #
-				#             PRACTICAL ASSIGNMENT 1            #
-				#                                               #
-				#           STUDENT: Jordi Bericat Ruz          #
-				#                                               #
-				#            FILE 1 OF 1: traceexit.c           #
-				#                                               #
-				#  IMPLEMENT A LINUX KERNEL MODULE THAT COUNTS 	#
-				#    EVERY EXIT SYSTEM CALL EXECUTED AND ALSO   #
-				#   ALLOWS ACCESS TO A SUMMARY FROM USER SPACE  #
-				#            INTERFACING VIA PROC FS            #
-				#                                               #
-				#                  Version 1.1                  #
-				#                                               #
-				#################################################
+ ###############################################################################
+ #                                                                             #
+ #				      	 UOC - Open University of Catalonia                    #
+ #                                                                             #
+ ###############################################################################
 
 
-###############################################################################
-#                                                                             #
-#  DESCRIPTION:                                                				  #
-#                                                                             #
-#                                                                             #
-#                                                                             #
-#  IMPLEMENTATION STRATEGY:                                                   #
-#                                                                             #
-#  Exit System calls monitoring                                               #
-#                                                                             #
-#  In regards to the exit syscall codes monitoring, we only have to call the  #
-#  low-level assembler code which performs the sys_exit call associated tasks #
-#  to a new function (new_sys_exit) and then link it to the system calls      #
-#  vector (sys_call_table). Summing-up: this way we "bypass" the original     #
-#  sys_exit call (original_sys_exit) so we can modify its behaviour (in our   #
-#  case, to keep track of every exit system call executed once the kernel     #
-#  module becomes enabled (insmod traceexit.ko).                              #
-#                                                                             #
-#  Procfs Interface implementation                                            #
-#                                                                             #
-#  The goal is to implement a communication interface between the user memory #
-#  space and the one associated with the kernel module, so an user would be   #
-#  able to read from the shell (user memory space) the data stored in the     #
-#  physical memory space reserved to the kernel (exit syscalls counters) via  #
-#  the procfs "virtual" interface -> cat "/proc/traceexit. Writing operations #
-#  will be performed from within the module (kernel memory space), so a       #
-#  writing interface from user to kernel space won't be needed.               #
-#                                                                             #
-#                                                                             #
-#  INPUT:  		 			 									              #
-#                                                                             #
-#  OUTPUT:                                                                    #
-#                                                                             #
-#  USAGE: See examples/usage.txt											  #
-#                                                                             #
-###############################################################################
+ ############################################################################### 
+ #                                                                             #
+ #				     	   OPERATIVE SYSTEMS DESIGN (DSO)                      #
+ #				               PRACTICAL ASSIGNMENT 1                          #
+ #                                                                             #
+ #				             STUDENT: Jordi Bericat Ruz                        #
+ #                                                                             #
+ #				              FILE 1 OF 1: traceexit.c                         #
+ #                                                                             #
+ #				                    Version 1.1                                #
+ #                                                                             #
+ ###############################################################################
+
+
+ ###############################################################################
+ #                                                                             #
+ #  DESCRIPTION:                                                               #
+ #                                                                             #
+ #  Implement a Linux Kernel module that keeps track of every exit system call #
+ #  executed and also allows access to a summary from user space interfacing   #
+ #  via procfs.                                                                #
+ #                                                                             #
+ #                                                                             #
+ #  IMPLEMENTATION STRATEGY:                                                   #
+ #                                                                             #
+ #  Exit System calls monitoring                                               #
+ #                                                                             #
+ #  In regards to the exit syscall codes monitoring, we only have to call the  #
+ #  low-level assembler code which performs the sys_exit call associated tasks #
+ #  to a new function (new_sys_exit) and then link it to the system calls      #
+ #  vector (sys_call_table). Summing-up: this way we "bypass" the original     #
+ #  sys_exit call (original_sys_exit) so we can modify its behaviour (in our   #
+ #  case, to keep track of every exit system call executed once the kernel     #
+ #  module becomes enabled (insmod traceexit.ko).                              #
+ #                                                                             #
+ #  Procfs Interface implementation                                            #
+ #                                                                             #
+ #  The goal is to implement a communication interface between the user memory #
+ #  space and the one associated with the kernel module, so an user would be   #
+ #  able to read from the shell (user memory space) the data stored in the     #
+ #  physical memory space reserved to the kernel (exit syscalls counters) via  #
+ #  the procfs "virtual" interface -> cat "/proc/traceexit. Writing operations #
+ #  will be performed from within the module (kernel memory space), so a       #
+ #  writing interface from user to kernel space won't be needed.               #
+ #                                                                             #
+ #                                                                             #
+ #  INPUT:                                                                     #
+ #                                                                             #
+ #  N/A                                                                        #
+ #                                                                             #
+ #                                                                             #
+ #  OUTPUT:                                                                    #
+ #                                                                             #
+ #  Summary at /proc/traceexit showing the amount of times every exit code     #
+ #  that has been invocated since the kernel module activation.                #
+ #                                                                             #
+ #                                                                             #
+ #  USAGE:                                                                     #
+ #                                                                             #
+ #  See examples/usage.txt                                                     #
+ #                                                                             #
+ ###############################################################################
 
 /
 # INICI DE L'SCRIPT
